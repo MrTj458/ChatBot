73401c8436c9bfadaba32617a869a36c30367afe
Trevor <thod0127@740s-pl-130.local>
2015-10-23 13:13:29 -0600

    The chatbot now chats.

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -5,24 +5,35 @@ import chat.model.Chatbot;
 
 /**
  * Controller for the Chatot project.
+ * 
  * @author thod0127
- * @version 1.2 10/23/15 Displays the Chatbot's userName variable.
+ * @version 1.3 10/23/15 The chatbot now chats.
  */
 public class ChatController
 {
 	private Chatbot simplebot;
 	private ChatView display;
-	
+
 	public ChatController()
 	{
 		display = new ChatView();
 		String user = display.getAnswer("What is your name?");
 		simplebot = new Chatbot(user);
 	}
-	
+
 	public void start()
 	{
 		display.displayPopup("Hello " + simplebot.getUserName() + ".");
+		chat();
 	}
-	
+
+	private void chat()
+	{
+		String textFromUser = display.getAnswer("Talk to the chatbot");
+		while (simplebot.lengthChecker(textFromUser))
+		{
+			display.displayPopup("wow " + textFromUser);
+		}
+	}
+
 }
--- src/chat/view/ChatView.java
+++ src/chat/view/ChatView.java
@@ -4,33 +4,38 @@ import javax.swing.JOptionPane;
 
 /**
  * This class provides GUI pop-ups for inputing and outputing Strings.
+ * 
  * @author thod0127
  * @version 1.0 10/21/15
  */
 public class ChatView
 {
-	
+
 	/**
 	 * Shows a InputDialog GUI pop-up to get an answer from the user.
-	 * @param displayMessage The message to be displayed.
+	 * 
+	 * @param displayMessage
+	 *            The message to be displayed.
 	 * @return answer as a String
 	 */
 	public String getAnswer(String displayMessage)
 	{
 		String answer = "";
-		
+
 		answer = JOptionPane.showInputDialog(null, displayMessage);
-		
+
 		return answer;
 	}
-	
+
 	/**
 	 * Shows a MessageDialog GUI pop-up to display text.
-	 * @param displayMessage The message to be displayed.
+	 * 
+	 * @param displayMessage
+	 *            The message to be displayed.
 	 */
 	public void displayPopup(String displayMessage)
 	{
 		JOptionPane.showMessageDialog(null, displayMessage);
 	}
-	
+
 }
