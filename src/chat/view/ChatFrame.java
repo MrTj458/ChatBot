60fcfeade34f46534177b9f49963e528bed1bb08
Trevor <thod0127@740s-pl-130.local>
2015-11-23 12:55:19 -0700

    Added ChatFrame and ChatPanel
    They do not contain anything.

---
+++ src/chat/view/ChatFrame.java
@@ -0,0 +1,27 @@
+package chat.view;
+
+import chat.controller.ChatController;
+import javax.swing.JFrame;
+
+public class ChatFrame extends JFrame
+{
+	private ChatController baseController;
+	private ChatPanel basePanel;
+	
+	public ChatFrame(ChatController baseController)
+	{
+		this.baseController = baseController;
+		basePanel = new ChatPanel(baseController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(basePanel);
+		this.setResizable(false);
+		this.setSize(400, 400);
+		this.setTitle("Chat Bot");
+		this.setVisible(true);
+	}
+}
---
+++ src/chat/view/ChatPanel.java
@@ -0,0 +1,34 @@
+package chat.view;
+
+import chat.controller.ChatController;
+import javax.swing.*;
+import java.awt.*;
+
+public class ChatPanel extends JPanel
+{
+	private ChatController baseController;
+	
+	public ChatPanel(ChatController baseController)
+	{
+		this.baseController = baseController;
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+}
