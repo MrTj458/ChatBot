6e8661f5e504d7dd843384653d97d8170f402297
Trevor <thod0127@740s-pl-130.local>
2016-03-07 12:10:40 -0700

    Created CTECTwitter and added the twitter4j library

---
+++ src/chat/model/CTECTwitter.java
@@ -0,0 +1,14 @@
+package chat.model;
+
+import java.util.ArrayList;
+import twitter4j.*;
+
+public class CTECTwitter
+{
+	private ArrayList<Status> statusList;
+	
+	public CTECTwitter()
+	{
+		
+	}
+}
