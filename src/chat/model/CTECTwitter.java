6a0fa4e3a6c33ebf50186b60c51d012b44ac236e
Trevor <thod0127@740s-pl-130.local>
2016-03-07 12:42:25 -0700

    added sendTweet method

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -6,8 +6,15 @@ import twitter4j.*;
 public class CTECTwitter
 {
 	private ArrayList<Status> statusList;
+	private ArrayList<String> wordsList;
 	
 	public CTECTwitter()
+	{
+		this.statusList = new ArrayList<Status>();
+		this.wordsList = new ArrayList<String>();
+	}
+	
+	public void sendTweet(String message)
 	{
 		
 	}
